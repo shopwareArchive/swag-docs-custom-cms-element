ae2730d2f2ecedb397ee6e00bc88725d1b1fef28
Jannis Leifeld <j.leifeld@shopware.com>
2019-10-24 07:55:33 +0200

    Update snippet extension with new syntax

--- src/Resources/administration/module/sw-cms/elements/youtube/index.js
+++ src/Resources/administration/module/sw-cms/elements/youtube/index.js
@@ -1,9 +1,8 @@
-import { Application } from 'src/core/shopware';
 import './component';
 import './config';
 import './preview';
 
-Application.getContainer('service').cmsService.registerCmsElement({
+Shopware.Service('cmsService').registerCmsElement({
     name: 'youtube',
     label: 'YouTube Video',
     component: 'sw-cms-el-youtube',
